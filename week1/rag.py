2cbbd0578fc761140f2f73e461b0d6e9164819a2
Lifan Sun <lifansun1412@gmail.com>
2026-02-22 23:52:28 -0800

    feat: add rag prompting

--- week1/rag.py
+++ week1/rag.py
@@ -37,7 +37,9 @@ QUESTION = (
 
 
 # TODO: Fill this in!
-YOUR_SYSTEM_PROMPT = ""
+YOUR_SYSTEM_PROMPT = """
+You are a senior software engineer in a top-tier tech company.
+"""
 
 
 # For this simple example
@@ -56,7 +58,7 @@ def YOUR_CONTEXT_PROVIDER(corpus: List[str]) -> List[str]:
 
     For example, return [] to simulate missing context, or [corpus[0]] to include the API docs.
     """
-    return []
+    return [corpus[0]]
 
 
 def make_user_prompt(question: str, context_docs: List[str]) -> str:
